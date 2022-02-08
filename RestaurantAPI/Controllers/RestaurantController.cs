8c080aa5712c404be00cbfe514b06e91aece46c2
Karol Ciesielski <karol.ciesielski@politech.pl>
2022-02-08 14:20:55 +0100

    create models, entities, services and controllers

---
+++ RestaurantAPI/Controllers/RestaurantController.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RestaurantAPI.Models;
+using RestaurantAPI.Services;
+using System.Collections.Generic;
+
+namespace RestaurantAPI.Controllers
+{
+    [Route("api/restaurant")]
+    public class RestaurantController : ControllerBase
+    {
+
+        private IRestaurantService _restaurantService;
+
+        public RestaurantController(IRestaurantService restaurantService)
+        {
+            _restaurantService = restaurantService;
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult Update([FromBody] UpdateRestaurantDto dto, [FromRoute]int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            var isUpdated = _restaurantService.Update(id, dto);
+
+            if(!isUpdated)
+            {
+                return NotFound();
+            }
+            return Ok();
+
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult Delete([FromRoute] int id)
+        {
+            var isDeleted = _restaurantService.Delete(id);
+
+            if (isDeleted)
+            { 
+                return NoContent();
+            }
+
+            return NotFound();
+        }
+
+        [HttpPost]
+        public ActionResult CreateRestaurant([FromBody] CreateRestaurantDto dto)
+        {
+            if(!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var id = _restaurantService.Create(dto);
+
+            return Created($"/api/restaurant/{id}", null);
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<RestaurantDto>> GetAll()
+        {
+            var restaurantsDtos = _restaurantService.GetAll();
+
+            return Ok(restaurantsDtos);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<RestaurantDto> Get([FromRoute] int id)
+        {
+            var restaurant = _restaurantService.GetById(id);
+            if(restaurant == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(restaurant);
+        }
+    }
+}
--- RestaurantAPI/Startup.cs
+++ RestaurantAPI/Startup.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using RestaurantAPI.Entities;
+using RestaurantAPI.Services;
 
 namespace RestaurantAPI
 {
@@ -22,6 +23,8 @@ namespace RestaurantAPI
             services.AddRazorPages();
             services.AddDbContext<RestaurantDbContext>();
             services.AddScoped<RestaurantSeeder>();
+            services.AddAutoMapper(this.GetType().Assembly);
+            services.AddScoped<IRestaurantService, RestaurantService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
