0ebbacd8cedccc851f568e76d2c65a38952f7a8e
Karol Ciesielski <karol.ciesielski@politech.pl>
2022-02-10 12:26:24 +0100

    Password validator, password hasher

--- RestaurantAPI/Services/AccountService.cs
+++ RestaurantAPI/Services/AccountService.cs
@@ -1,4 +1,5 @@
-﻿using RestaurantAPI.Entities;
+﻿using Microsoft.AspNetCore.Identity;
+using RestaurantAPI.Entities;
 using RestaurantAPI.Models;
 
 namespace RestaurantAPI.Services
@@ -10,10 +11,12 @@ namespace RestaurantAPI.Services
     public class AccountService : IAccountService
     {
         private readonly RestaurantDbContext _context;
+        private readonly IPasswordHasher<User> _passwordHasher;
 
-        public AccountService(RestaurantDbContext context)
+        public AccountService(RestaurantDbContext context, IPasswordHasher<User> passwordHasher)
         {
             _context = context;
+            _passwordHasher = passwordHasher;
         }
         public void RegisterUser(RegisterUserDto dto)
         {
@@ -24,7 +27,9 @@ namespace RestaurantAPI.Services
                 Nationality = dto.Nationality,
                 RoleId = dto.RoleId
             };
+            var hashedPassword = _passwordHasher.HashPassword(newUser, dto.Password);
 
+            newUser.PasswordHash = hashedPassword;
             _context.Users.Add(newUser);
             _context.SaveChanges();
         }
