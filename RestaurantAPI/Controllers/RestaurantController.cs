e3cf0d956bed68f4a8bd0e980bb6b8a35145e100
Karol Ciesielski <karol.ciesielski87@gmail.com>
2022-02-28 17:46:26 +0100

    RestaurantQuery added

--- RestaurantAPI/Controllers/RestaurantController.cs
+++ RestaurantAPI/Controllers/RestaurantController.cs
@@ -52,9 +52,9 @@ namespace RestaurantAPI.Controllers
 
         [HttpGet]
         [Authorize(Policy = "CreatedAtLeast2Restaurants")]
-        public ActionResult<IEnumerable<RestaurantDto>> GetAll([FromQuery]string searchPhrase)
+        public ActionResult<IEnumerable<RestaurantDto>> GetAll([FromQuery]RestaurantQuery query)
         {
-            var restaurantsDtos = _restaurantService.GetAll(searchPhrase);
+            var restaurantsDtos = _restaurantService.GetAll(query);
 
             return Ok(restaurantsDtos);
         }
